a318a6f948f55a017267bb608fc86e37cf3d9145
jvyden <jvyden@jvyden.xyz>
2025-08-08 22:13:51 -0400

    text/uri-list test

--- ClipboardTest/Program.cs
+++ ClipboardTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using SDL;
+﻿using System.Text;
+using SDL;
 
 Console.WriteLine("Clipboard test!");
 SDL3.SDL_Init(SDL_InitFlags.SDL_INIT_VIDEO); // doesn't spawn a window
@@ -16,8 +17,22 @@ Console.WriteLine("\tPrimary selection: " + SDL3.SDL_GetPrimarySelectionText());
 Console.WriteLine($"Clipboard has image/png data? {(bool)SDL3.SDL_HasClipboardData("image/png")}");
 ThrowIfSdlError();
 
-Console.WriteLine($"Clipboard has text/uri-list data? {(bool)SDL3.SDL_HasClipboardData("text/uri-list")}");
-ThrowIfSdlError();
+SDLBool hasUriList = SDL3.SDL_HasClipboardData("text/uri-list");
+Console.WriteLine($"Clipboard has text/uri-list data? {(bool)hasUriList}");
+
+unsafe
+{
+    nuint len = 0;
+    if (hasUriList)
+    {
+        IntPtr ptr = SDL3.SDL_GetClipboardData("text/uri-list", &len);
+        ThrowIfSdlError();
+        Span<byte> span = new((void*)ptr, (int)len);
+        Console.WriteLine($"\tClipboard text: {Encoding.UTF8.GetString(span)}, len: {(int)len}");
+    }
+}
+
+Console.WriteLine($"Clipboard has fake data? {(bool)SDL3.SDL_HasClipboardData("text/not-real-mime")}");
 
 Console.WriteLine("Setting clipboard data...");
 SDL3.SDL_SetClipboardText("Testy test");
